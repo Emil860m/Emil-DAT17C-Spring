531a6db8ceb908815661f025478e493f4f365adb
Emil860m <fenger.emil@gmail.com>
2018-10-25 11:08:10 +0200

    Update src/main/java/dk/kea/swc3/dat17c/demo/CustomErrors/ErrorController.java

--- src/main/java/dk/kea/swc3/dat17c/demo/CustomErrors/ErrorController.java
+++ src/main/java/dk/kea/swc3/dat17c/demo/CustomErrors/ErrorController.java
@@ -1,15 +1,15 @@
-package dk.kea.swc3.dat17c.demo.CustomErrors;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class ErrorController {
-
-    @GetMapping("/error")
-    public String Error(){
-        return "error";
-    }
-
-
-}
+//package dk.kea.swc3.dat17c.demo.CustomErrors;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ErrorController {
+//
+//    @GetMapping("/error")
+//    public String Error(){
+//        return "error";
+//    }
+//
+//
+//}
