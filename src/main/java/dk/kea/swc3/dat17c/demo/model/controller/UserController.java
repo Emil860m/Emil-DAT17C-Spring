c5c41feacdab56561dee0815e2d8232605ded141
Emil860m <fenger.emil@gmail.com>
2018-10-25 11:05:30 +0200

    Initial commit

---
+++ src/main/java/dk/kea/swc3/dat17c/demo/model/controller/UserController.java
@@ -0,0 +1,25 @@
+package dk.kea.swc3.dat17c.demo.model.controller;
+
+import dk.kea.swc3.dat17c.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    //@ResponseBody
+    public ModelAndView index(@RequestParam(defaultValue = "{{user}}") String name,
+                              @RequestParam(defaultValue = "-1") Integer age,
+                              @RequestParam(defaultValue = "F") String gender)
+    {
+        Character gender1 = gender.charAt(0);
+        User u = new User(name, age, gender1);
+        ModelAndView mv = new ModelAndView("index");
+        mv.getModel().put("user", u);
+        return mv;
+    }
+
+}
